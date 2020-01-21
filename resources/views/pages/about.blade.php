96a3b9cea4ef3467dac761001f5217c96ab27f4f
Zachary Rener <me@zacharyrener.com>
2020-01-21 02:22:15 -0600

    Added routes for three pages using a pages controller, added 3 views,  added  Bootstrap and navbar

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,6 @@
-About
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>About page - Lorem ipsum dolor sit amet</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 Route::get('/', 'PagesController@index');
 
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
